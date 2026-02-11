c9baef49bab6bb196ebaea619af4a95c3bcd4b38
uppermooninfinity <uppermooninfinity@gmail.com>
2026-02-11 18:35:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 #--------------------------------
 API_ID = 39679517
 API_HASH = "aed61e5ff8c711895f8b0c99e51c16cc"
-BOT_TOKEN = ""
+BOT_TOKEN = "8451766490:AAFbP9bWAEWr9nGoSb8rRM5LyidxYLR3r-g"
 MONGO_DB_URI = "mongodb+srv://knight4563:knight4563@cluster0.a5br0se.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 #--------------------------------
@@ -18,7 +18,7 @@ STORAGE_CHANNEL_ID = -1003852280111
 AUTHORS = [7651303468]
 
 #--------------------------------
-String_client_1 = ""
+String_client_1 = "AQJddh0APZpFgUYUqjpz_R2XxA0qiVoUT1aQlM-07mWV-b0Cdp3-nn6KtmeInHqSgdTz-5JYMnRcl6dEcvJwlvPPHybwjOdBrKVSAJsBLP-B5QqRdOJPRCVKysXh2V66nWcdwSKQ-YGQtPJYEflBRCaz8avq9paDm_hop6D7i68wS2VXY64Lkhs5_Cl4z1aDH7cqwTpXhlpvae1eUg76hYY0_fprQHq06xFd3zNtyS2gfx5ruvfBp39G3WHx9OLkBdOS6DL6SunhOMF9qoQxBLJvphbAhcMJIDNSwlh-s44udKLEZjl5fsx1HzTPyJfasohJmq9kO0Jk-ZvtMULreoRxbnC16gAAAAHZk9LIAAAQIP33AANEizZ3BxPJi5EJg2Iyr75eKBEMT2CtpM2RFBIfOXeBVGgTj2fVVGLS2mDimjibRErtVS1ZDRJ3RwcIG8RrOiEsr95lcGZh_8MMBuB1usugJd5ILTmavBSNAdXokceODHf1CGC3uOnlSw_8oEAkHk8O7tu_V3LUfNHYZ33aRW-cabo3R9U5KL349PcrgyF4UhOF77RROS6nRIegO0G1rtQHEFslaWJ27opnD2pVmOYnr3ToLIykCAgHsH3lZnNFehwp_viBAaqnH650x4UEiJfRkZha6mEa1tdtJ_2pzOMEg9LoR2yahLvG_Cd_MKVVOZAwWE17SoqQ64O6QZtNchrgAAAAHN07JeAA"
 String_client_2 = ""
 String_client_3 = ""
 Mustjoin = "snowy_hometown"
